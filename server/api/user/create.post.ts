512faca6339cc0f0e9e45808aff01af4d7782404
幻梦晓寒_kaka <97349507+Dreamkaka@users.noreply.github.com>
2024-05-06 23:49:16 +0800

    Update create.post.ts

--- server/api/user/create.post.ts
+++ server/api/user/create.post.ts
@@ -1,17 +1,8 @@
 export default defineEventHandler(async event => {
-	const { data } = useFetch('/api/user/create', {
-    method: 'POST',
-    body: {
-      nickname: 'nickname',
-      password: 'password'
-    } catch (error) {
+	 try {
+		// return await new UserSchema(body).save();
+		return { code: 401, msg: 'reject' };
+	} catch (error) {
 		return new Response(error as string, { status: 500 });
 	}
-  });
-	// try {
-		// return await new UserSchema(body).save();
-//		return { code: 401, msg: 'reject' };
-//	} catch (error) {
-//		return new Response(error as string, { status: 500 });
-//	}
 
